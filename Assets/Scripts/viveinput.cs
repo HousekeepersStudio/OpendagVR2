27d864dfe80c958b6e7b1d6e49b41de80471925e
Unknown <vuurenskoen@gmail.com>
2017-11-24 11:39:47 +0100

    First push
    This push contains everything we got

---
+++ Assets/Scripts/viveinput.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class viveinput : MonoBehaviour {
+    private void Awake()
+    {
+        SteamVR_TrackedController buttons = GetComponent<SteamVR_TrackedController>();
+    }
+}
