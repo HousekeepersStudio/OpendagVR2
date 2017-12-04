813405f07ca48849deb0019c6441dce38e1f53af
Unknown <vuurenskoen@gmail.com>
2017-12-04 11:59:27 +0100

    Teleport and Temporary Weapon Switch

---
+++ Assets/Scripts/TeleportZoneEnabler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportZoneEnabler : MonoBehaviour {
+
+    GameObject cameraRig;
+
+    GameObject zone;
+
+    private void Awake()
+    {
+        cameraRig = GameObject.Find("[CameraRig]");
+        zone = this.gameObject.transform.Find("TeleportZone").gameObject;
+    }
+
+    private void Update()
+    {
+        if (!cameraRig.GetComponentInChildren<Teleportation>().enabled)
+        {
+            zone.SetActive(false);
+        }
+        else
+        {
+            zone.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/temp_WeaponSwitch.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp_WeaponSwitch : MonoBehaviour {
+    public GameObject controllerLeft;
+    public GameObject controllerRight;
+
+    SteamVR_TrackedController buttonsLeft;
+    SteamVR_TrackedController buttonsRight;
+
+    bool teleport = false;
+    bool grab = true;
+
+    private void Awake()
+    {
+        buttonsLeft = controllerLeft.GetComponent<SteamVR_TrackedController>();
+        buttonsRight = controllerRight.GetComponent<SteamVR_TrackedController>();
+        ChangeWeapon();
+    }
+
+    private void FixedUpdate()
+    {
+        if (buttonsLeft.gripped || buttonsRight.gripped)
+        {
+            StartCoroutine(WeaponSwitchDelay());
+        }
+    }
+    
+    void ChangeWeapon()
+    {
+        teleport = !teleport;
+        grab = !grab;
+
+        if (teleport)
+        {
+            if (!controllerRight.GetComponent<Teleportation>().enabled)
+            {
+                controllerRight.GetComponent<SteamVR_LaserPointer>().enabled = true;
+                controllerRight.transform.Find("New Game Object").gameObject.SetActive(true);
+                controllerRight.GetComponent<Teleportation>().enabled = true;
+                controllerRight.GetComponent<RWVR_InteractionController>().enabled = false;
+                controllerLeft.GetComponent<RWVR_InteractionController>().enabled = false;
+                controllerRight.transform.Find("Origin").gameObject.SetActive(false);
+                controllerLeft.transform.Find("Origin").gameObject.SetActive(false);
+            }
+        }
+
+        if (grab)
+        {
+            if (!controllerRight.GetComponent<RWVR_InteractionController>().enabled)
+            {
+                controllerRight.GetComponent<SteamVR_LaserPointer>().enabled = false;
+                controllerRight.transform.Find("New Game Object").gameObject.SetActive(false);
+                controllerRight.GetComponent<Teleportation>().enabled = false;
+                controllerRight.GetComponent<RWVR_InteractionController>().enabled = true;
+                controllerLeft.GetComponent<RWVR_InteractionController>().enabled = true;
+                controllerRight.transform.Find("Origin").gameObject.SetActive(true);
+                controllerLeft.transform.Find("Origin").gameObject.SetActive(true);
+
+            }
+        }
+    }
+    
+    IEnumerator WeaponSwitchDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ChangeWeapon();
+    }
+}
