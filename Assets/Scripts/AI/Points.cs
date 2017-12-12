408d2266d5041b48db6db0a5b544f123e0a73239
Unknown <vuurenskoen@gmail.com>
2017-12-12 11:46:50 +0100

    WIP Pointssytem

---
+++ Assets/Scripts/AI/Points.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PointssSystem : MonoBehaviour {
+
+    public int score = 20;
+    public int rewardPerBowKill = 10;
+    public int rewardPerTurretKill = 5;
+    
+    public void AddPoints(string typeOfKill)
+    {
+        if(typeOfKill == "Bow" || typeOfKill == "bow")
+        {
+            score += rewardPerBowKill;
+            Debug.Log(score);
+
+        }
+        else if(typeOfKill == "Turret" || typeOfKill == "turret"){
+            score += rewardPerTurretKill;
+
+        }
+        else
+            Debug.Log("The given string is not an option!");
+    }
+
+    void SubmitScore(string name)
+    {
+        // Submit score to database
+    }
+}
