2c3ace8f88af822497c61561105a34b82c77f7e8
Unknown <vuurenskoen@gmail.com>
2017-12-14 09:18:53 +0100

    torch script
    torch script

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -3,76 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Torch : MonoBehaviour {
-
-    private int amountOfBanners = 4;
-    private bool dragonBanner = true;
-    private bool vikingBanner = true;
-    private bool ravenBanner = true;
-    private bool serpantBanner = true;
-
-    private void OnCollisionEnter(Collision col)
-    {
-        if(col.gameObject.name == "dragonBanner")
-        {
-            Destroy(col.gameObject);
-            amountOfBanners -= 1;
-            dragonBanner = false;
-        }
-        if (col.gameObject.name == "ravenBanner")
-        {
-            Destroy(col.gameObject);
-            amountOfBanners -= 1;
-            ravenBanner = false;
-        }
-        if (col.gameObject.name == "vikingBanner")
-        {
-            Destroy(col.gameObject);
-            amountOfBanners -= 1;
-            vikingBanner = false;
-        }
-        if (col.gameObject.name == "serpantBanner")
-        {
-            Destroy(col.gameObject);
-            amountOfBanners -= 1;
-            serpantBanner = false;
-        }
-
-        if (amountOfBanners == 1)
-        {
-            SetHouse();
-            LoadIntroScene();
-        }
-    }
-
-    private void SetHouse()
-    {
-        if (serpantBanner)
-        {
-            PlayerPrefs.SetString("house", "serpents");
-        }
-        else if (vikingBanner)
-        {
-            PlayerPrefs.SetString("house", "vikings");
-        }
-        else if (dragonBanner)
-        {
-            PlayerPrefs.SetString("house", "dragons");
-        }
-        else if (ravenBanner)
-        {
-            PlayerPrefs.SetString("house", "ravens");
-        }
-    }
-
-    private void LoadIntroScene()
-    {
-        SceneManager.LoadScene(1);
-    }
-}
-using UnityEngine.SceneManagement;
-
-public class Torch : MonoBehaviour {
+public class Torch : MonoBehaviour
+{
 
     private int amountOfBanners = 4;
     private bool dragonBanner = true;
@@ -87,7 +19,7 @@ public class Torch : MonoBehaviour {
 
     private void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.name == "dragonBanner")
+        if (col.gameObject.name == "dragonBanner")
         {
             GameObject.Instantiate(particle, col.gameObject.transform);
             Destroy(col.gameObject);
@@ -115,6 +47,8 @@ public class Torch : MonoBehaviour {
             amountOfBanners -= 1;
             serpantBanner = false;
         }
+    }
+
     private void FixedUpdate()
     {
         if (amountOfBanners == 1)
@@ -123,6 +57,8 @@ public class Torch : MonoBehaviour {
             SetHouse();
             LoadIntroScene();
         }
+    }
+
     private void BrightenFlag()
     {
         if (serpantBanner)
@@ -177,3 +113,5 @@ public class Torch : MonoBehaviour {
         FadeOut();
         SceneManager.LoadScene(1);
     }
+
+}
