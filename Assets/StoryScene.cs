bfa51f25716ee2bbc7a135a931ce6bba8cbe3703
Unknown <vuurenskoen@gmail.com>
2017-12-22 10:20:54 +0100

    0.2-alpha: To 0.3-alpha

--- Assets/StoryScene.cs
+++ Assets/StoryScene.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class StoryScene : MonoBehaviour {
     void Update () {
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Target");
         bool targetsAlive = false;
-        foreach(GameObject target in targets)
+        if(targets != null)
         {
-            if (target.activeSelf)
-                targetsAlive = true;
+            foreach (GameObject target in targets)
+            {
+                if (target.activeSelf)
+                    targetsAlive = true;
+            }
+
+            if (!targetsAlive)
+            {
+                SceneManager.LoadScene(2);
+            }
         }
 	}
 }
