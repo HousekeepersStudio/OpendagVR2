d8eed616d46b4a8793fcbbfff5216f380a58a9a8
Unknown <d228214@edu.rocwb.nl>
2018-01-18 11:53:59 +0100

    Auto stash before merge of "Feature-UpgradeTower" and "origin/Feature-UpgradeTower"

--- OpendagVR/Assets/Scripts/UpgradeTower.cs
+++ OpendagVR/Assets/Scripts/UpgradeTower.cs
@@ -1,16 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradeTower : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public GameObject controllerRight;
+    public string towerTag;
+
+    private SteamVR_LaserPointer pointer;
+    private SteamVR_TrackedController buttons;
+
+    void Update()
+    {
+        try
+        {
+            if (pointer != null && buttons != null)
+                Raycast();
+
+            if (pointer == null)
+                pointer = controllerRight.GetComponent<SteamVR_LaserPointer>();
+
+            if (buttons == null)
+                buttons = controllerRight.GetComponent<SteamVR_TrackedController>();
+        }
+        catch (Exception e)
+        {
+            //Debug.Log(e);
+        }
+    }
+
+    void Raycast()
+    {
+        RaycastHit hit;
+        if (pointer.holder.activeSelf)
+        {
+            Physics.Raycast(pointer.pointer.transform.position, pointer.pointer.transform.forward, out hit);
+            if (hit.collider.tag == towerTag && buttons.triggerPressed)
+            {
+                if (true)
+                {
+                    Turret t = hit.transform.GetChild(1).GetComponent<Turret>();
+                }
+            }
+        }
+    }
 }
