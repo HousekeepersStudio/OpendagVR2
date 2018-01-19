5353bcfee9b58102928d5f241ee0b6e115bfc634
Cyri <staal.yp@gmail.com>
2018-01-19 11:15:29 +0100

    Fixed new models of Enemies & Animations
    Do not access "Feature-EnemyAnimation" as it is corrupted. Use this branch from now on!

--- OpendagVR/Assets/Scripts/AI/StandardEnemy.cs
+++ OpendagVR/Assets/Scripts/AI/StandardEnemy.cs
@@ -1,56 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class StandardEnemy : Enemy {
-    public string mainTowerObjectName;
     NavMeshAgent nav;
     static string enemyType = "Standard";
     static int baseHealth = 100;
-    static float baseDamage = 2f;
+    static int baseDamage = 2;
     static float sHealthMultiplier = healthMultiplier;
     static float sDamageMultiplier = damageMultiplier;
 
     static int startLevel = 1;
     static int health = (int)((baseHealth * sHealthMultiplier) * startLevel);
     static int enemyDamage = (int)((baseDamage * sDamageMultiplier) * startLevel);
-    public bool mainTowerAttack = false;
-    private bool isAttacking = false;
-    private GameObject mainTower;
+
+    Animator animator;
+    bool mainTowerAttack = false;
 
     public StandardEnemy() : base(enemyType, health, enemyDamage, startLevel)
     {
-		
+
     }
 
     private void Awake()
     {
         healthBar = this.transform.Find("HealthBarCanvas").Find("HealthBG").Find("HealthBar").GetComponent<Image>();
         agent = GetComponent<NavMeshAgent>();
+        animator = this.GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (mainTower == null)
-            mainTower = GameObject.Find(mainTowerObjectName);
-
-
-        if (!isAttacking)
-        {
-            if (mainTowerAttack && mainTower != null)
-            {
-                Attack();
-                isAttacking = true;
-            }
-        }
-        else
-        {
-            if (!mainTowerAttack && mainTower != null)
-            {
-                isAttacking = false;
-            }
-        }
-        
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            curHealth -= 100;
 
         if(curHealth <= 0)
         {
@@ -59,16 +42,7 @@ public class StandardEnemy : Enemy {
         else
         {
             if (agent.enabled && agent.isOnNavMesh)
-            {
-                targets = GameObject.FindGameObjectsWithTag("Target");
-                if (targets.Length > 0)
-                {
-                    MoveTo(targets[0]);
-                    mainTowerObjectName = targets[0].name;
-                }
-            }
-                
-
+                MoveTo(GameObject.FindGameObjectWithTag("Target"));
 
             if (!agent.isOnNavMesh)
             {
@@ -78,22 +52,33 @@ public class StandardEnemy : Enemy {
                 NavMesh.SamplePosition(gameObject.transform.position, out closesthit, 500f, NavMesh.AllAreas);
                 transform.position = closesthit.position;
                 agent.isStopped = false;
-                targets = GameObject.FindGameObjectsWithTag("Target");
-                MoveTo(targets[0]);
+                MoveTo(GameObject.FindGameObjectWithTag("Target"));
             }
         }
     }
 
     private void Die()
     {
-        mainTowerAttack = false;
         StartCoroutine(DieT(gameObject));
-
     }
 
-    private void Attack()
+    private void OnTriggerEnter(Collider other)
     {
-        StopMove();
-        StartCoroutine(EnemyAttackTower(mainTowerAttack, mainTower.GetComponent<Collider>()));
+        if (other.gameObject.tag == "Target")
+        {
+            StopMove();
+            animator.SetTrigger("Attack");
+            mainTowerAttack = true;
+            StartCoroutine(EnemyAttackTower(mainTowerAttack, other));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Target")
+        {
+            animator.SetTrigger("Walk");
+            mainTowerAttack = false;
+        }
     }
 }
