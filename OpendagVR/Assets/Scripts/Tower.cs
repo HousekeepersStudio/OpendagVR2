dd1d803f18aa4e603b936d9940ab3a927099d9d1
Unknown <d229297@edu.rocwb.nl>
2018-01-19 10:04:52 +0100

    Added my stuff and changed tower.cs

--- OpendagVR/Assets/Scripts/Tower.cs
+++ OpendagVR/Assets/Scripts/Tower.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Tower : MonoBehaviour {
-    public float health;
-    public Tower target;
+public class Tower : Entity {
+    private Tower target;
+    public Text text;
 
-	// Use this for initialization
-	void Start () {
-		
+    public Tower(string type, float maxHealth, float damage, int level, Tower target)
+        :base (type, maxHealth, damage, level)
+    {
+        this.target = target;
+    }
+    
+    // Use this for initialization
+    void Start () {
+        this.target = this.GetComponent<Tower>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        text.text = this.level.ToString();
 	}
 }
